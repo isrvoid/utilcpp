c30ddac1490b639e2f21861c6cbe4bbbd47b95f0
Johannes Teichrieb <isrvoid@gmail.com>
2016-07-16 21:01:53 +0200

    Build unittest unconditionally

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,18 +1,18 @@
-set(TEMP_GCC_WARNINGS "-Wall -Wextra -pedantic -Wshadow -Wpointer-arith -Wcast-align -Wwrite-strings \
--Wredundant-decls -Wnested-externs -Winline -Wno-long-long -Wconversion -Wstrict-prototypes")
-set(TEMP_GPP_WARNINGS "-Wall -Wextra -pedantic -Wpointer-arith -Wcast-align -Wwrite-strings \
--Wredundant-decls -Winline -Wno-long-long -Wno-conversion -Wzero-as-null-pointer-constant")
-#FIXME remove "no" from -Wno-conversion
+set(TEMP_GCC_WARNINGS "-Wall -Wextra -pedantic -Wshadow -Wpointer-arith -Wcast-align -Wwrite-strings -Wredundant-decls -Wnested-externs -Winline -Wno-long-long -Wconversion -Wstrict-prototypes")
+set(TEMP_GPP_WARNINGS "-Wall -Wextra -pedantic -Wpointer-arith -Wcast-align -Wwrite-strings -Wredundant-decls -Winline -Wno-long-long -Wno-conversion -Wzero-as-null-pointer-constant")
+# TODO remove "no" from -Wno-conversion
 
-# repeating default flags is probably unnecessary; are they always empty?
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 ${TEMP_GCC_WARNINGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 ${TEMP_GPP_WARNINGS}")
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 14)
 
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -std=c11 ${TEMP_GCC_WARNINGS}")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -std=c++14 ${TEMP_GPP_WARNINGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TEMP_GCC_WARNINGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TEMP_GPP_WARNINGS}")
 
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -std=c11 ${TEMP_GCC_WARNINGS}")
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -std=c++14 ${TEMP_GPP_WARNINGS}")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${TEMP_GCC_WARNINGS}")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${TEMP_GPP_WARNINGS}")
+
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${TEMP_GCC_WARNINGS}")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${TEMP_GPP_WARNINGS}")
 
 # build type macro definitions for the linker
 if(${CMAKE_BUILD_TYPE} MATCHES "Release")
@@ -22,8 +22,4 @@ endif()
 
 if(${CMAKE_BUILD_TYPE} MATCHES "Debug")
 	add_definitions(-DDEBUG)
-endif()
-
-if(${UNITTEST})
-	add_definitions(-DUNITTEST)
 endif()
