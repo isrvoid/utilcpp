2d5af485d58e684dcd5b176fc25ab4b1376b7221
Johannes Teichrieb <isrvoid@gmail.com>
2019-10-23 14:57:20 +0200

    Remove namespace from aspecttype

--- src/util/aspecttype.h
+++ src/util/aspecttype.h
@@ -3,15 +3,11 @@
 #include <type_traits>
 
 #define ASPECT_TYPE(ASPECT, TYPE)  template<> \
-    struct util::_AspectTypeSpec<decltype(ASPECT), ASPECT> { \
+    struct _AspectTypeSpec<decltype(ASPECT), ASPECT> { \
         using type = TYPE; \
     };
-
-namespace util {
 
 template<typename Aspect, Aspect val, typename = std::enable_if_t<std::is_enum<Aspect>::value>>
 struct _AspectTypeSpec {
     using type = void;
 };
-
-} // namespace util
